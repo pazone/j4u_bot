456db385808924a3f9557a13ad55a2aeb8c4b994
Space Cat <77073354+njy@users.noreply.github.com>
2022-05-10 21:35:57 +0200

    convert to lambda

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 require('dotenv').config()
 const notion = require('./notion')
 const telegram = require('./telegram')
-const cron = require('node-cron');
 
-publishJobs = async () => {    
+module.exports.publishJobs = async () => {    
     console.info("Publishing new jobs")
     
-    jobs = await notion.fetchNewJobs()    
+    const jobs = await notion.fetchNewJobs()    
     telegram.publishToChannel(jobs)    
     notion.markAsPublished(jobs)
 
     console.info(`Published ${jobs.length} jobs`)
 }
 
-//every 3 hours
-var pubTask = cron.schedule('0 0 */3 * * *', publishJobs)
-pubTask.start()
-
-process.once('SIGINT', () => {
-    pubTask.stop()
-})
-
-process.once('SIGTERM', () => {    
-    pubTask.stop()
-})
